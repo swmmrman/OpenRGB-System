e655208a6f922ffbdf591daffaec33e24f7f5bea
Stephan Kern <stephan@swmmrman.com>
2022-08-27 13:27:11 -0600

    Setting up for system monitor borrowing from g910-cpu-graph.

---
+++ main.py
@@ -0,0 +1,27 @@
+#!/bin/env.? python3
+from openrgb import OpenRGBClient
+from openrgb.utils import RGBColor, DeviceType
+from time import sleep
+import signal
+
+RUNNING = True
+
+def sig_handler(sig, frame):
+  global RUNNING
+  RUNNING = False
+
+
+BACKGROUND = "562500"
+START_COLOR = "210000"
+KEYS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',]
+
+
+signal.signal(signal.SIGINT, sig_handler)
+signal.signal(signal.SIGTERM, sig_handler)
+
+cli = OpenRGBClient()
+keyboard = cli.get_devices_by_type(DeviceType.KEYBOARD)[0]
+keyboard.clear()
+key_name_list = [ key.name[5:] for key in keyboard.leds ]
+
